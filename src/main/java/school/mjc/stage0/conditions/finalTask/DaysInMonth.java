4c66d349ad9fd87db6959f0624caddff6efc2da4
SardorAKayumov <sardor.zeroz@gmail.com>
2022-10-03 12:57:33 +0500

    commit

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if(year < 1 || month < 1 || month > 12) {
+            System.out.println("invalid date");
+            return;
+        }
 
+        boolean isLeapYear = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+
+        int daysInMonth = switch (month) {
+            case 2 -> isLeapYear ? 29 : 28;
+            case 4, 6, 9, 11 -> 30;
+            default -> 31;
+        };
+
+        System.out.println(daysInMonth);
     }
 }
